eabd40c7daa2cd95485fed757e0b0ee679eb29df
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-08-26 13:59:05 +0300

    projects animation

--- my-portfolio/src/Components/Projects.tsx
+++ my-portfolio/src/Components/Projects.tsx
@@ -12,6 +12,11 @@ export default function Projects({ projects, description }: ProjectsProps) {
   const ref = useRef<HTMLDivElement>(null)
   const [visible, setVisible] = useState(false)
 
+  const projectRefs = useRef<(HTMLDivElement | null)[]>([])
+  const [visibleProjects, setVisibleProjects] = useState<boolean[]>(
+    Array(projects.length).fill(false)
+  )
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
@@ -30,6 +35,29 @@ export default function Projects({ projects, description }: ProjectsProps) {
     return () => observer.disconnect()
   }, [])
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const index = Number(entry.target.getAttribute('data-index'))
+          if (entry.isIntersecting) {
+            setVisibleProjects((prev) => {
+              const newState = [...prev]
+              newState[index] = true
+              return newState
+            })
+            observer.unobserve(entry.target)
+          }
+        })
+      },
+      { threshold: 0.2 }
+    )
+
+    projectRefs.current.forEach((el) => el && observer.observe(el))
+
+    return () => observer.disconnect()
+  }, [projects])
+
   return (
     <div className="projectsContainer">
       <div ref={ref} className={visible ? 'slide-up' : 'hidden'}>
@@ -44,12 +72,28 @@ export default function Projects({ projects, description }: ProjectsProps) {
           </div>
         </div>
       </div>
-      <div className="projects">
+      {/* <div className="projects">
         {projects.map((project, index) => (
           <div
             key={index}
             className={`project-wrapper ${
               visible ? `fade-in delay-${index}` : 'hidden'
+            }`}
+          >
+            <Project project={project} />
+          </div>
+        ))}
+      </div> */}
+      <div className="projects">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            data-index={index}
+            ref={(el) => {
+              projectRefs.current[index] = el
+            }}
+            className={`project-wrapper ${
+              visibleProjects[index] ? 'fade-in' : 'hidden'
             }`}
           >
             <Project project={project} />
