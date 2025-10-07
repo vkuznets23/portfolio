0c9d2c2bc0f7dcfd5206e1616c3b31f1c5b37a4c
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-10-07 20:16:01 +0300

    trying to improve performance

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,25 +1,24 @@
 import { useGlobal, useAppData } from './hooks'
-// import {
-//   FirstScreen,
-//   Marquee,
-//   Projects,
-//   Experience,
-//   Footer,
-//   Navbar,
-//   AboutMe,
-//   ErrorComponent,
-// } from './Components'
+import {
+  FirstScreen,
+  Marquee,
+  Projects,
+  Experience,
+  Footer,
+  Navbar,
+  AboutMe,
+  ErrorComponent,
+} from './Components'
 import './App.css'
-import React, { Suspense } from 'react'
-import ErrorComponent from './Components/Error'
+// import React, { Suspense } from 'react'
 
-const AboutMe = React.lazy(() => import('./Components/AboutMe'))
-const Navbar = React.lazy(() => import('./Components/Navbar'))
-const Footer = React.lazy(() => import('./Components/Footer'))
-const Experience = React.lazy(() => import('./Components/Experience'))
-const Projects = React.lazy(() => import('./Components/Projects'))
-const Marquee = React.lazy(() => import('./Components/Marquee'))
-const FirstScreen = React.lazy(() => import('./Components/FirstScreen'))
+// const AboutMe = React.lazy(() => import('./Components/AboutMe'))
+// const Navbar = React.lazy(() => import('./Components/Navbar'))
+// const Footer = React.lazy(() => import('./Components/Footer'))
+// const Experience = React.lazy(() => import('./Components/Experience'))
+// const Projects = React.lazy(() => import('./Components/Projects'))
+// const Marquee = React.lazy(() => import('./Components/Marquee'))
+// const FirstScreen = React.lazy(() => import('./Components/FirstScreen'))
 
 export default function App() {
   const { language } = useGlobal()
@@ -43,33 +42,31 @@ export default function App() {
 
   return (
     <main className="main-container">
-      <Suspense fallback={<span className="loader"></span>}>
-        <Navbar />
-        <div className="first-wrapper">
-          <FirstScreen />
-        </div>
-        <div className="content-wrapper">
-          <Marquee
-            text={
-              language === 'en'
-                ? ' && about me >> about me || about me * about me '
-                : ' * обо мне >> обо мне || обо мне * обо мне && обо мне >> обо мне '
-            }
-          />
-          <Experience />
-          <Marquee
-            text={
-              language === 'en'
-                ? ' projects && projects * projects >> projects && projects * projects >>'
-                : ' проекты && проекты * проекты >> проекты && проекты * проекты >>'
-            }
-            style="1.95deg"
-          />
-          <Projects />
-          <AboutMe />
-          <Footer />
-        </div>
-      </Suspense>
+      <Navbar />
+      <div className="first-wrapper">
+        <FirstScreen />
+      </div>
+      <div className="content-wrapper">
+        <Marquee
+          text={
+            language === 'en'
+              ? ' && about me >> about me || about me * about me '
+              : ' * обо мне >> обо мне || обо мне * обо мне && обо мне >> обо мне '
+          }
+        />
+        <Experience />
+        <Marquee
+          text={
+            language === 'en'
+              ? ' projects && projects * projects >> projects && projects * projects >>'
+              : ' проекты && проекты * проекты >> проекты && проекты * проекты >>'
+          }
+          style="1.95deg"
+        />
+        <Projects />
+        <AboutMe />
+        <Footer />
+      </div>
     </main>
   )
 }
