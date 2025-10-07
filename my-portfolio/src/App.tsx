1ef9c0a128ee8dc64b7a655b0ca51acba18b6a5b
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-10-07 20:56:52 +0300

    try

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,15 +1,23 @@
 import { useGlobal, useAppData } from './hooks'
 import {
+  // Marquee,
+  // Projects,
+  // Experience,
+  // Footer,
+  // Navbar,
+  // AboutMe,
   FirstScreen,
-  Marquee,
-  Projects,
-  Experience,
-  Footer,
-  Navbar,
-  AboutMe,
   ErrorComponent,
 } from './Components'
 import './App.css'
+import React, { Suspense } from 'react'
+
+const AboutMe = React.lazy(() => import('./Components/AboutMe'))
+const Navbar = React.lazy(() => import('./Components/Navbar'))
+const Footer = React.lazy(() => import('./Components/Footer'))
+const Experience = React.lazy(() => import('./Components/Experience'))
+const Projects = React.lazy(() => import('./Components/Projects'))
+const Marquee = React.lazy(() => import('./Components/Marquee'))
 
 export default function App() {
   const { language } = useGlobal()
@@ -38,24 +46,30 @@ export default function App() {
         <FirstScreen />
       </div>
       <div className="content-wrapper">
-        <Marquee
-          text={
-            language === 'en'
-              ? ' && about me >> about me || about me * about me '
-              : ' * обо мне >> обо мне || обо мне * обо мне && обо мне >> обо мне '
-          }
-        />
-        <Experience />
-        <Marquee
-          text={
-            language === 'en'
-              ? ' projects && projects * projects >> projects && projects * projects >>'
-              : ' проекты && проекты * проекты >> проекты && проекты * проекты >>'
-          }
-          style="1.95deg"
-        />
-        <Projects />
-        <AboutMe />
+        <Suspense fallback={<span className="loader"></span>}>
+          <Marquee
+            text={
+              language === 'en'
+                ? ' && about me >> about me || about me * about me '
+                : ' * обо мне >> обо мне || обо мне * обо мне && обо мне >> обо мне '
+            }
+          />
+          <Experience />
+        </Suspense>
+        <Suspense fallback={<span className="loader"></span>}>
+          <Marquee
+            text={
+              language === 'en'
+                ? ' projects && projects * projects >> projects && projects * projects >>'
+                : ' проекты && проекты * проекты >> проекты && проекты * проекты >>'
+            }
+            style="1.95deg"
+          />
+          <Projects />
+        </Suspense>
+        <Suspense fallback={<span className="loader"></span>}>
+          <AboutMe />
+        </Suspense>
         <Footer />
       </div>
     </main>
