d13637c89018535705b5cc271ae24104310badeb
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-10-07 14:46:25 +0300

    better

--- my-portfolio/src/Components/logo.tsx
+++ my-portfolio/src/Components/logo.tsx
@@ -8,8 +8,8 @@ export const Logo = () => {
 
   return (
     <svg
-      width="97"
-      height="50"
+      width="80"
+      height="35"
       viewBox="0 0 97 50"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
