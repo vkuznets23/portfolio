f99f3d4be23868801830e363ce343614509ebaa0
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-10-08 10:43:55 +0300

    better?

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -4,4 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Разделяем vendor библиотеки
+          'react-vendor': ['react', 'react-dom'],
+          'marquee-vendor': ['react-fast-marquee'],
+          'typewriter-vendor': ['react-simple-typewriter'],
+          'utils-vendor': ['typograf'],
+        },
+      },
+    },
+    // Минификация и tree-shaking
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true, // Удаляем console.log в production
+        drop_debugger: true,
+      },
+    },
+    // Оптимизация chunk size
+    chunkSizeWarningLimit: 1000,
+  },
 })
