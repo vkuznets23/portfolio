14c57ebe21f6af8484434b7d74d84866eb39a686
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2026-03-04 14:11:48 +0200

    change the text

--- my-portfolio/src/data/experience.ts
+++ my-portfolio/src/data/experience.ts
@@ -26,7 +26,7 @@ export const experienceData = {
         date: 'Nov 2021 - Jun 2022',
         name: 'UX/UI designer at Func.',
         description:
-          'Built apps and websites in an Agile-like environment using no-code tools. Applied custom CSS and JavaScript to enhance functionality and design. Managed database setup. Handled app store publishing and deployment',
+          'Developed web and mobile applications in an Agile-like environment using no-code tools (applying CSS and blueprint code). Managed database configuration and structure. Participated in client meetings to align business requirements with technical solutions. Created interactive prototypes and conducted user testing to validate concepts before development. Managed app store submission and deployment processes.',
       },
       {
         date: 'Apr 2024 - October 2025',
@@ -35,10 +35,16 @@ export const experienceData = {
           'Developed solo and group projects in C, programmed from scratch or using minimal libraries. Gained foundational knowledge in C++ basics. Completed a web development project, applying front-end and back-end concepts. Participated in a mentorship program, collaborating with a mentor to build Camagru, deepening understanding of front-end development, API integration, state management, and building production-ready projects',
       },
       {
-        date: 'Nov 2024 - present',
+        date: 'Nov 2024 - Mar 2025',
         name: 'Full Stack Open by University of Helsinki 🇫🇮',
         description:
           'Completed comprehensive full-stack web development training focusing on modern JavaScript, React, Redux, NodeJS, and Express. Gained hands-on experience with REST APIs, GraphQL, and database integration (MongoDB, PostgreSQL). Learned state management, component architecture, and hooks in React. Practiced testing, CI/CD pipelines, and deployment of production-ready applications.Explored TypeScript, async programming, and best practices for scalable web apps',
+      },
+      {
+        date: 'Aug 2025 - present',
+        name: 'Vocational qualification in ICT at Business College 🇫🇮',
+        description:
+          'Completed programming studies through Hive Helsinki, developing strong analytical and problem-solving skills. Built solid foundations in entrepreneurship, business strategy, and value creation. Strengthened professional communication skills in Finnish and English. Developed a strong understanding of sustainability, social responsibility, and modern business practices. Enhanced teamwork, self-leadership, and the ability to operate effectively in dynamic environments.',
       },
     ],
   },
@@ -57,7 +63,7 @@ export const experienceData = {
         date: 'Ноябрь 2021 - Июнь 2022',
         name: 'UX/UI дизайнер в Func.',
         description:
-          'Создавала приложения и сайты в среде, похожей на Agile, используя no-code инструменты. Применяла кастомный CSS и JavaScript для улучшения функциональности и дизайна. Настраивала базы данных. Отвечала за публикацию и деплой приложений в App Store.',
+          'Разрабатывала веб- и мобильные приложения в Agile-подобной среде с использованием no-code инструментов. Настраивала и управляла структурой базы данных. Участвовала в клиентских встречах для согласования бизнес-требований и технических решений. Создавала интерактивные прототипы и проводила пользовательское тестирование для проверки гипотез перед разработкой. Осуществляла публикацию и деплой приложений в app stores.',
       },
       {
         date: 'Апрель 2024 - Октябрь 2025',
