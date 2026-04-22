73ab5e97f948c8f74de7951d2aae911b548801ff
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2026-04-22 09:17:27 +0300

    data change

--- my-portfolio/src/Components/experience/EducationLinkedCard.tsx
+++ my-portfolio/src/Components/experience/EducationLinkedCard.tsx
@@ -31,9 +31,7 @@ export default function EducationLinkedCard({
         <span className="education-card-date">
           {hasRangeDate ? (
             <>
-              {firstDatePart}
-              <br />
-              {secondDatePart}
+              {firstDatePart} - {secondDatePart}
             </>
           ) : (
             item.date
